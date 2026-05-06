4b2c62da1f54fdda29d5144e09c8dd3a25a3df74
MeatballRoland <meatballrolland@gmail.com>
2026-05-06 19:50:35 +0100

    test?

--- corknotes/src/main/java/com/corknotes/Main.java
+++ corknotes/src/main/java/com/corknotes/Main.java
@@ -1,7 +1,19 @@
 package com.corknotes;
 
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import com.corknotes.view.MainWindow;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception ignored) {
+            }
+
+            MainWindow window = new MainWindow();
+            window.setVisible(true);
+        });
     }
 }
